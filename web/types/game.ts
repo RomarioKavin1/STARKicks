47e1819a452baf397d4b18ccfe46ab84b2c27c30
RomarioKavin1 <romario7kavin@gmail.com>
2025-01-14 01:34:20 +0530

    game types

---
+++ web/types/game.ts
@@ -0,0 +1,32 @@
+// types/game.ts
+export type Card = {
+  id: string;
+  name: string;
+  attack: number;
+  control: number;
+  defense: number;
+  position: "attack" | "midfield" | "defense";
+  specialAbility?: {
+    name: string;
+    cost: number;
+    effect: string;
+  };
+  image?: string;
+};
+
+export type GameState = {
+  playerEnergy: number;
+  opponentEnergy: number;
+  playerScore: number;
+  opponentScore: number;
+  currentTurn: "player" | "opponent";
+  playerCards: Card[];
+  opponentCards: Card[];
+  selectedCard?: Card;
+};
+
+export type GameAction = {
+  type: "ATTACK" | "SPECIAL" | "SWITCH_POSITION" | "END_TURN";
+  card?: Card;
+  target?: Card;
+};
